dbbb0a30e4ef8ee0c5493963e7bccbc2589e47a8
spongzi <spongzi@outlook.com>
2025-09-10 18:18:20 +0800

    功能：添加用于用户事件的webhook路由并更新next.config.ts
    - 实现了一个新的webhook路由，用于处理来自Clerk的user.created、user.updated和user.deleted事件。
    - 更新了next.config.ts，以允许来自ngrok的开发源。
    - 在package.json中添加了用于并发开发和ngrok设置的新npm脚本。
    - 包含了concurrently包，用于在开发过程中管理多个进程。

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* config options here */
+  allowedDevOrigins: ["https://achlorhydric-obnoxiously-kacy.ngrok-free.app"],
 };
 
 export default nextConfig;
